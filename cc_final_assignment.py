20afe9519fc633b0b0ca971b49e818a70eb73cf2
catsmey <soycatherine@zohomail.com>
2026-01-06 14:57:56 +0100

    final algorithm

--- cc_final_assignment.py
+++ cc_final_assignment.py
@@ -5,13 +5,15 @@ import pathlib
 import io
 from PIL import Image
 import random
+from tqdm import tqdm
 
 scrapbook_styles = ["normal","vintage", "nature", "whimsical", "cute/kawaii", "futuristic", "maximalist", "gothic", "pop art"]
         
-def generate_initial_population(client, text, images, analysis, n, styles):
+def generate_initial_population(client, text, image_urls, n, styles):
     out_dir = pathlib.Path("results") / "generation0"
     out_dir.mkdir(parents=True, exist_ok=True)
-    for i in range(n):
+    
+    for i in tqdm(range(n), desc="Generating initial population", unit="scrapbook"):
         style = styles[i]
         print(f"Generating scrapbook in {style} style...")
         design_prompt = f"""
@@ -20,7 +22,6 @@ def generate_initial_population(client, text, images, analysis, n, styles):
         Play with layering papers, photographs, and embellishments.
         Create the scrapbook in a {style} scrapbook style.
         Use this trip summary and the attached photos to guide composition:
-
         """
         user_content = [{"type": "input_text", "text": text}]
         for url in image_urls:
@@ -40,6 +41,7 @@ def generate_initial_population(client, text, images, analysis, n, styles):
             if output.type == "image_generation_call"
         ]
 
+        # save and show image
         if image_data:
             image_base64 = image_data[0]
             out_path = out_dir / f"scrapbook{i+1}.png"
@@ -47,40 +49,8 @@ def generate_initial_population(client, text, images, analysis, n, styles):
                 f.write(base64.b64decode(image_base64))
             Image.open(io.BytesIO(base64.b64decode(image_base64))).show()
 
-# def generate_initial_population(client, text, images, analysis, n, styles):
-#     out_dir = pathlib.Path("results") / "generation0"
-#     out_dir.mkdir(parents=True, exist_ok=True)
-#     for i in range(n):
-#         style = styles[i]
-#         print(f"Generating scrapbook in {style} style...")
-#         design_prompt = f"""
-#         Design a high-resolution travel journal scrapbook page as an image.
-#         Include the attached photos and add elements such as paper textures, fabrics, ribbons, natural elements like leaves and flowers, tape, hand-drawn accents, short texts describing the experience (not the input text), collected travel items like postcards or maps, stickers, and more typical scrapbook elements. 
-#         Play with layering papers, photographs, and embellishments.
-#         Create the scrapbook in a {style} scrapbook style.
-#         Use this trip summary, the extracted analysis and the attached photos to guide composition:
-
-#         Trip summary:
-#         {text}
-
-#         Analysis:
-#         {json.dumps(analysis, ensure_ascii=False)}
-
-#         """
-#         result = client.images.edit(
-#             model="gpt-image-1.5",
-#             image=[open(p, "rb") for p in images],
-#             prompt=design_prompt,
-#             n=1,
-#         )
-
-#         img_bytes = base64.b64decode(result.data[0].b64_json)
-#         out_path = out_dir / f"scrapbook{i+1}.png"
-#         with out_path.open("wb") as f:
-#             f.write(img_bytes)
-#         Image.open(io.BytesIO(img_bytes)).show()
-
 def encode_images(image_paths):
+    # encode images to base64 data URLs to send to openai API
     image_urls = []
     for p in image_paths:
         with open(p, "rb") as image_file:
@@ -90,6 +60,7 @@ def encode_images(image_paths):
     return image_urls
 
 def downscale_images(image_paths):
+    # if images are too large they are downscaled to save memory and costs
     for p in image_paths:
         img = Image.open(p)
         # Downscale if larger than max_side.
@@ -101,92 +72,90 @@ def downscale_images(image_paths):
             img = img.resize(new_size, Image.LANCZOS)
         img.save(p)
 
-def analyze_emotions_and_info(client, text, image_urls):
-    analysis_prompt = """
-You are a travel-journal emotion and content analyst.
-Given the user’s travel text and photos, extract structured information to guide scrapbook design.
-
-Return STRICT JSON ONLY with this schema:
-{
-  "emotions_overall": ["joy|serenity|awe|nostalgia|..."],
-  "sentiment": "positive|neutral|negative",
-  "key_locations": [str],
-  "key_events": [str],
-  "color_palette": [{"hex": "#RRGGBB"}],
-  "season": [str]
-}
-
-Constraints:
-- Make color palette align with emotions and imagery.
-- JSON only. No markdown. No extra commentary.
-"""
-    user_content = [{"type": "input_text", "text": text}]
-    for url in image_urls:
-        user_content.append({"type": "input_image", "image_url": url})
-    result = client.responses.create(
-        model="gpt-5.1",
-        input=[
-            {"role": "system", "content": [{"type": "input_text", "text": analysis_prompt}]},
-            {"role": "user", "content": user_content},
-        ],
-    )
-    #print(result.output_text)
-    answer = result.output_text
-    try:
-        analysis = json.loads(answer)
-    except json.JSONDecodeError:
-        print("Error exptracting information. Try again.")  
-    return analysis
-
-def crossover():
+def compare_scrapbooks(parent_urls):
     prompt = """
-    Combine the two provided scrapbooks to create a new design. Do not modify the original photos."""
-    return prompt
+    You are given two previously generated travel scrapbook designs, both created from the same base prompt.
 
-def mutation():
-    #2 types of mutation: style change or composition change 0.5 probability each
-    if random.random() < 0.5:
-        style = random.choice(scrapbook_styles)
-        prompt = f"""
-        Modify the provided scrapbook by changing its style to {style}. Do not modify the original photos."""
-    else:
-        prompt = """
-        Modify the provided scrapbook by changing composition/arrangement of the elements. Do not change any of the individual components."""
-    return prompt
+    Your task is NOT to redesign the page or rewrite the full prompt.
+    Instead, carefully analyze both scrapbook designs and extract their strongest shared and complementary visual components.
 
-def evolutionairy_algorithm(client, history, n, num_generations):
+    Write short, concise instruction lines that can be added to the original prompt to guide the generation of a new scrapbook design that incorporates elements from both designs.
+    Do NOT include any instuctions about the contents of the photographs as these must remain unchanged.
+    
+    Focus on:
+    - Layout structure (photo placement, balance, layering)
+    - Repeating motifs or embellishments (e.g. tape, frames, icons, pressed plants)
+    - Color palette and material textures
+    - Typography and caption style
+    - Mood and storytelling approach
+
+    Do not repeat or paraphrase the original prompt.
+    Do not describe the existing designs.
+
+    Output only a short bullet list of additive instructions, written as prompt-ready imperatives (e.g. “Emphasize…”, “Blend…”, “Incorporate…”).
+
+    The instructions should be general enough to apply to new images, but specific enough to clearly merge the visual DNA of both scrapbook designs.
+    """
+    user_content = []
+    for url in parent_urls:
+        user_content.append({"type": "input_image", "image_url": url})
+    
+    resp = client.responses.create(
+                model="gpt-5.1",
+                input=[
+                    {"role": "system", "content": [{"type": "input_text", "text": prompt}]},
+                    {"role": "user", "content": user_content},
+                ],
+            )
+    return resp.output_text
+    
+def evolutionary_algorithm(client, text, image_urls, n, num_generations):
     mutation_rate = 0.7
-    crossover_rate = 0.7
+    crossover_rate = 0.5
     # pick n random styles for initial population
     styles = random.sample(scrapbook_styles, n)
-    #generate_initial_population(client, text, images, analysis, n, styles)
+    generate_initial_population(client, text, image_urls, n, styles)
     
     for generation in range(0, num_generations):
         print(f"Generation {generation} completed. Please select the two best designs from the 'results/generation{generation}' folder and enter their numbers separated by commas. To improve them further.")
+        
+        # let user select parents
         selected = input("Enter favorite scrapbooks: ").split(",")
-        # retrieve the images from results/generation{generation} to use as parents
         parent_images = []
         for s in selected:
             parent_path = pathlib.Path("results") / f"generation{generation}" / f"scrapbook{int(s)}.png"
             parent_images.append(parent_path)
         parent_urls = encode_images(parent_images)
+        
         # create next generation
         out_dir = pathlib.Path("results") / f"generation{generation+1}"
         out_dir.mkdir(parents=True, exist_ok=True)
-        for i in range(n):
+        added_prompt = compare_scrapbooks(parent_urls)
+        for i in tqdm(range(n), desc=f"Generation {generation+1}", unit="scrapbook"):
+            user_content = [
+                {"type": "input_text", "text": text},
+                *[{"type": "input_image", "image_url": u} for u in image_urls],
+            ]
             if random.random() < crossover_rate:
                 # crossover: mix elements from both parents
-                prompt = crossover() 
-                user_content = []
-                for url in parent_urls:
-                    user_content.append({"type": "input_image", "image_url": url})
+                prompt = f"""
+                            You are given a trip description and original trip photos (ground truth).
+                            Design a travel journal scrapbook page as an image.
+                            Include the attached photos and add elements such as paper textures, fabrics, ribbons, natural elements like leaves and flowers, tape, hand-drawn accents, short texts describing the experience (not the input text), collected travel items like postcards or maps, stickers, and more typical scrapbook elements. 
+                            Play with layering papers, photographs, and embellishments.
+                            Favor richness over simplicity: add layered papers, visible textures, torn edges, and other scrapbook elementsThe result should feel handcrafted, tactile, and densely detailed rather than minimal or flat.
+                            Use the trip summary and the attached photos to guide composition.
+                            Do NOT redraw, recolor, or reinterpret the photos.
+                            Use the following additional instructions to guide your design:
+                            
+                            {added_prompt}
+                            """
             else:
                 # mutation: 1. change style 2. change composition
                 if random.random() < mutation_rate:
                     parent = random.choice(parent_urls)
                     prompt = mutation()
-                    user_content = [{"type": "input_image", "image_url": parent}]
-                # original parent is kept
+                    user_content.append({"type": "input_image", "image_url": parent})
                 else:
                     # save parent image as is to next generation
                     parent = random.choice(parent_images)
@@ -194,20 +163,13 @@ def evolutionairy_algorithm(client, history, n, num_generations):
                     with parent.open("rb") as source, out_path.open("wb") as destination:
                         destination.write(source.read())
                     continue
-                
-            messages = history + [
-                {
-                    "role": "system",
-                    "content": [{"type": "input_text", "text": prompt}],
-                },
-                {
-                    "role": "user",
-                    "content": user_content,
-                },
-            ]   
+            
             resp = client.responses.create(
                 model="gpt-5.1",
-                input=messages,
+                input=[
+                    {"role": "system", "content": [{"type": "input_text", "text": prompt}]},
+                    {"role": "user", "content": user_content},
+                ],
                 tools=[{"type": "image_generation", "model": "gpt-image-1-mini"}],
             )
         
@@ -223,6 +185,46 @@ def evolutionairy_algorithm(client, history, n, num_generations):
                 with out_path.open("wb") as f:
                     f.write(base64.b64decode(image_base64))
                 Image.open(io.BytesIO(base64.b64decode(image_base64))).show()
+       
+def mutation():
+    #2 types of mutation: style change or composition change 0.5 probability each
+    if random.random() < 0.5:
+        style = random.choice(scrapbook_styles)
+        prompt = f"""
+            You are given:
+            - The original trip description and original trip photos (ground truth).
+            - A scrapbook design created from this trip.
+
+            Your task:
+            Modify the scrapbook by changing its decorative style to a "{style}" scrapbook aesthetic.
+
+            How to change the style:
+            - Adjust colors, textures, embellishments, and overall visual theme to match the "{style}" style.
+            - Add elements such as paper textures, fabrics, ribbons, natural elements like leaves and flowers, tape, hand-drawn accents, short texts describing the experience (not the input text), collected travel items like postcards or maps, stickers, and more typical scrapbook elements. 
+            - Replace or restyle decorative elements to fit the new style.
+            - Maintain a dense, layered scrapbook feel.
+
+            Do NOT redraw, recolor or reinterpret the photos.
+            """
+
+    else:
+        prompt = """
+        You are given:
+        - The original trip description and original trip photos (ground truth).
+        - A scrapbook design created from this trip.
+
+        Your task:
+        Modify the scrapbook by changing the composition and arrangement of elements.
+
+        How to change the composition:
+        - Maintain the same aesthetic style
+        - Change the position, angles, and of photos and decorative elements
+        - Experiment with different layering, overlaps, depth, spacing, and alignment.
+        - Preserve and emphasize small details such as paper edges, textures, and hand-drawn accents.
+
+        Do NOT redraw, recolor, or reinterpret the photos.
+        """
+    return prompt
     
 def get_initial_user_input():
     print("To create a new travel yournal page, please place the desired images (to be included in the scrapbook) and a txt file containing a description of your trip in a folder in the same directory as this script. Please enter the folder name below.")
@@ -238,38 +240,12 @@ def get_initial_user_input():
     image_paths = list(folder_path.glob("*.jpg")) + list(folder_path.glob("*.png"))
     return text, image_paths
 
-def create_history(text, image_urls):
-    # gives the model some context to its goal
-    base_prompt = """You are a travel scrapbook designer. 
-    You create scrapbook pages as images about a user's travel experience based on provided photos and a description of the trip. 
-    You add typical scrapbook elements such as paper textures, fabrics, ribbons, natural elements like leaves and flowers, tape, hand-drawn accents, short texts describing the experience (not the input text), collected travel items like postcards or maps, stickers, and more typical scrapbook elements.
-    You play with layering papers, photographs, and embellishments.
-    Only create scrapbook pages that keep the original trip photos and story as the core content.
-    """
-    history = [
-        {
-            "role": "system",
-            "content": [{"type": "input_text", "text": base_prompt}],
-        },
-        {
-            "role": "user",
-            "content": (
-                [{"type": "input_text", "text": text}]
-                + [{"type": "input_image", "image_url": u} for u in image_urls]
-            ),
-        },
-    ]
-    return history
-
 if __name__ == "__main__": 
     client = OpenAI()   
     text, images = get_initial_user_input()
     downscale_images(images)
     image_urls = encode_images(images)
-    #analysis = analyze_emotions_and_info(client, text, image_urls)
-    analysis= {}
-    history = create_history(text, image_urls)
-    evolutionairy_algorithm(client, history, n=4, num_generations=3)
+    evolutionary_algorithm(client, text, image_urls, n=4, num_generations=3)
 
     
     
